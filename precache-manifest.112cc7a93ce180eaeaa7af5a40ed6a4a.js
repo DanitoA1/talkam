de0356aa69a5fd8fe19d5ec72216f48b8f98275c
DanitoA1 <ahiabadaniel@gmail.com>
2020-10-22 09:57:37 +0100

    gh-pages

---
+++ precache-manifest.112cc7a93ce180eaeaa7af5a40ed6a4a.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc2340cf12f644fe006",
+    "url": "/talkam/css/app.3637b459.css"
+  },
+  {
+    "revision": "9bbe090690f7d2b197a4",
+    "url": "/talkam/css/chunk-vendors.dd6f2d35.css"
+  },
+  {
+    "revision": "313a65630d341645c13e4f2a0364381d",
+    "url": "/talkam/fonts/Roboto-Black.313a6563.woff"
+  },
+  {
+    "revision": "59eb3601394dd87f30f82433fb39dd94",
+    "url": "/talkam/fonts/Roboto-Black.59eb3601.woff2"
+  },
+  {
+    "revision": "cc2fadc3928f2f223418887111947b40",
+    "url": "/talkam/fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
+    "url": "/talkam/fonts/Roboto-BlackItalic.f75569f8.woff2"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "/talkam/fonts/Roboto-Bold.50d75e48.woff"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/talkam/fonts/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "4fe0f73cc919ba2b7a3c36e4540d725c",
+    "url": "/talkam/fonts/Roboto-BoldItalic.4fe0f73c.woff"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "/talkam/fonts/Roboto-BoldItalic.94008e69.woff2"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/talkam/fonts/Roboto-Light.c73eb1ce.woff"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/talkam/fonts/Roboto-Light.d26871e8.woff2"
+  },
+  {
+    "revision": "13efe6cbc10b97144a28310ebdeda594",
+    "url": "/talkam/fonts/Roboto-LightItalic.13efe6cb.woff"
+  },
+  {
+    "revision": "e8eaae902c3a4dacb9a5062667e10576",
+    "url": "/talkam/fonts/Roboto-LightItalic.e8eaae90.woff2"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/talkam/fonts/Roboto-Medium.1d659482.woff"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/talkam/fonts/Roboto-Medium.90d16760.woff2"
+  },
+  {
+    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
+    "url": "/talkam/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+  },
+  {
+    "revision": "83e114c316fcc3f23f524ec3e1c65984",
+    "url": "/talkam/fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/talkam/fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/talkam/fonts/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "/talkam/fonts/Roboto-RegularItalic.4357beb8.woff2"
+  },
+  {
+    "revision": "f5902d5ef961717ed263902fc429e6ae",
+    "url": "/talkam/fonts/Roboto-RegularItalic.f5902d5e.woff"
+  },
+  {
+    "revision": "ad538a69b0e8615ed0419c4529344ffc",
+    "url": "/talkam/fonts/Roboto-Thin.ad538a69.woff2"
+  },
+  {
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "/talkam/fonts/Roboto-Thin.d3b47375.woff"
+  },
+  {
+    "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
+    "url": "/talkam/fonts/Roboto-ThinItalic.5b4a33e1.woff2"
+  },
+  {
+    "revision": "8a96edbbcd9a6991d79371aed0b0288e",
+    "url": "/talkam/fonts/Roboto-ThinItalic.8a96edbb.woff"
+  },
+  {
+    "revision": "541e65fb46e44ecdf7a9da8a9b881446",
+    "url": "/talkam/fonts/materialdesignicons-webfont.541e65fb.eot"
+  },
+  {
+    "revision": "c61b9c12f68ee1ba045a4b49dba29ca5",
+    "url": "/talkam/fonts/materialdesignicons-webfont.c61b9c12.woff2"
+  },
+  {
+    "revision": "fc03f7f15facede623faa7666c7d1f5a",
+    "url": "/talkam/fonts/materialdesignicons-webfont.fc03f7f1.ttf"
+  },
+  {
+    "revision": "ff13d121c1a1cf74d8e06bd39e1746c3",
+    "url": "/talkam/fonts/materialdesignicons-webfont.ff13d121.woff"
+  },
+  {
+    "revision": "25773ff0d2518b7d6fcf038c4b872be5",
+    "url": "/talkam/img/SVG/boardfinish.svg"
+  },
+  {
+    "revision": "1a7b91a0dc8e987b6d649d270ef968cb",
+    "url": "/talkam/img/SVG/boardshare.svg"
+  },
+  {
+    "revision": "4bec411ee5cf346b95e81ea2b40ca317",
+    "url": "/talkam/img/SVG/boardstart.svg"
+  },
+  {
+    "revision": "10ad989c4b934c878aed12a372c99a19",
+    "url": "/talkam/img/SVG/boardwelcome.svg"
+  },
+  {
+    "revision": "a33893988905df3f6fa5a496f21474e0",
+    "url": "/talkam/img/SVG/feebackwelcome.svg"
+  },
+  {
+    "revision": "c22c2c4eb9d6430ab05d17ade1b1d6bc",
+    "url": "/talkam/img/SVG/feedback.svg"
+  },
+  {
+    "revision": "056f22f7e3099a9eb5599a08941e60ef",
+    "url": "/talkam/img/SVG/feedbackwelcome.svg"
+  },
+  {
+    "revision": "d0577cb2e547064a23ac05432b587308",
+    "url": "/talkam/img/SVG/mobile_life.svg"
+  },
+  {
+    "revision": "0e58142dda2ed24fc5f31509445f2c5e",
+    "url": "/talkam/img/SVG/quiz.svg"
+  },
+  {
+    "revision": "398ac7cc1089537e02f1a57350b57e4d",
+    "url": "/talkam/img/SVG/reviews.svg"
+  },
+  {
+    "revision": "96df3290dde01088ff70836992f5a25b",
+    "url": "/talkam/img/SVG/steps.svg"
+  },
+  {
+    "revision": "f97eec7bbd888e58669d2caa4fe62fed",
+    "url": "/talkam/img/gold.jpg"
+  },
+  {
+    "revision": "7a63bd7334e1f28eef181e6bc7567082",
+    "url": "/talkam/img/habiba.jpg"
+  },
+  {
+    "revision": "bcca1bc5b503b2b969ea54cb94c9a587",
+    "url": "/talkam/index.html"
+  },
+  {
+    "revision": "cbc2340cf12f644fe006",
+    "url": "/talkam/js/app.f18c17f6.js"
+  },
+  {
+    "revision": "9bbe090690f7d2b197a4",
+    "url": "/talkam/js/chunk-vendors.507a0ce3.js"
+  },
+  {
+    "revision": "a146b0a52ccc190f92929e255ef0d3f5",
+    "url": "/talkam/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/talkam/robots.txt"
+  }
+]);
